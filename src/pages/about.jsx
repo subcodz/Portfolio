96afaa6fa7c1e93f6294baf235da8f8d9abbd9f6
Subhajit Baidya <subhajitsb923692@gmail.com>
2026-02-04 01:27:09 +0530

    font problem fix

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,7 +9,7 @@ export default function About() {
     return (
         <div className=" w-full min-h-screen gap-5 flex select-none justify-center relative pt-25 pb-25">
             <div className="sm:grid sm:grid-cols-[1fr_0.6fr] gap-3 w-79 sm:w-fit">    
-                <div className="grid sm:grid-rows-[0.6fr] gap-3">    
+                <div className="grid sm:grid-rows-[0.5fr] gap-3">    
                     <div
                         className="
                             px-6 flex flex-col
